1be751238384d926aa12fea75ad9fd846697b671
suhelen <magichuihui@gmail.com>
2026-07-18 22:32:47 +0800

    Add GPLv3 license and update all Go source headers (#9)

--- pkg/os/syscall_unix.go
+++ pkg/os/syscall_unix.go
@@ -1,5 +1,23 @@
 //go:build linux || darwin || freebsd || netbsd || openbsd
 
+// gmake - Go port of GNU Make
+//
+// Copyright (C) 1988-2022 Free Software Foundation, Inc.
+// Copyright (C) 2026 amyinfo
+//
+// gmake is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// gmake is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with gmake.  If not, see <https://www.gnu.org/licenses/>.
+
 package os
 
 import (
